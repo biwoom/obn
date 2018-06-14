f1a9caef204898733f5d6f89e17b0b3fb5a5e7f4
biwoom <bingeoul@gmail.com>
2018-06-14 23:10:18 +0000

    chagne login signup function - no email auth

--- wiki/forms.py
+++ wiki/forms.py
@@ -669,11 +669,19 @@ class UserCreationForm(UserCreationForm):
                 raise forms.ValidationError(
                     "Thank you, non-human visitor. Please keep trying to fill in the form.")
         return cd
-
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError(u'Email 주소가 이미 존재합니다')
+        return email
+        
     class Meta:
         model = User
         fields = ("username", "email")
 
+        
 class UserUpdateForm(forms.ModelForm):
     # password1 = forms.CharField(label="New password", widget=forms.PasswordInput(), required=False)
     # password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput(), required=False)
